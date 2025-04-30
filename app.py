35cddda11c922441dfa7de8b38adba4b8643fe82
Kevin Tan <kevintannnn05@gmail.com>
2025-04-30 14:04:16 +0800

    re-routed app.py so that the landing page is not the dashboard

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
